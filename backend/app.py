ea859fe2218e1f7a85738cb388530b33252ca160
nivas25 <nivas3347r@gmail.com>
2025-10-24 14:25:55 +0530

    Updated app.py with CORS

--- backend/app.py
+++ backend/app.py
@@ -2,9 +2,11 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask, jsonify
+from flask_cors import CORS
 from routes.auth_routes import auth_bp
 load_dotenv()
 app = Flask(__name__)
+CORS(app, resources={r"/auth/*": {"origins": "http://localhost:3000"}})
 
 # ----------------------------------------------
 
